f3e70de6d2ddd8753269c1cf8f09a1cc7ea4c96a
MohammadParvez <mohammadparvez00966@gmail.com>
2022-05-20 23:18:01 +0600

    componet tree

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import MainComponent from './components/MainComponent';
 function App() {
   return (
     <div className="App">
+     <MainComponent/>
     
     </div>
   );
